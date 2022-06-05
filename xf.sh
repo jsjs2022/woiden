91da701bdcb19e538e1b2bc63a41ecdc6d1106d8
jsjs2022 <88915097+jsjs2022@users.noreply.github.com>
2022-06-06 00:11:14 +0800

    Update xf.sh

--- xf.sh
+++ xf.sh
@@ -60,7 +60,7 @@ cat <<EOF >/etc/v2ray/config.json
             "settings": {
                 "clients": [
                     {
-                        "id": "11111111-1111-1111-1111-111111111111",
+                        "id": "e99562fc-1665-4df3-885d-9f178642388a",
                         "alterId": 0
                     }
                 ],
